9e9086234b9cac2a7e1c0976f2d97aca030dfba6
Donny Davis <donnywdavis@icloud.com>
2015-04-10 15:31:45 -0400

    Add list for ships and a dictionary with ship details

--- battleship.py
+++ battleship.py
@@ -6,6 +6,30 @@ grid = []
 for index in range(10):
     grid.append(["O"] * 10)
 
+# List of ships for the board
+shipsList = ["battleship", "carrier", "submarine"]
+# Dictionary of ship details
+ships = {
+    "battleship": {
+        "length": 4,
+        "direction": "",
+        "start": [],
+        "end": []
+    },
+    "carrier": {
+        "length": 6,
+        "direction": "",
+        "start": [],
+        "end": []
+    },
+    "submarine": {
+        "length": 3,
+        "direction": "",
+        "start": [],
+        "end": []
+    }
+}
+
 
 # Draw the board to the screen
 def draw_board(board):

06644ca3cf417ae507c6e810f31d01860e61503b
Donny Davis <donnywdavis@icloud.com>
2015-04-13 17:34:39 -0400

    Add main loop to let the player enter a row and column along with initial edit checks

--- battleship.py
+++ battleship.py
@@ -3,6 +3,8 @@ __project__ = 'Battleship'
 
 from random import randint
 
+print("Let's play Battleship!")
+
 # Create our 10x10 grid for the game board
 grid = []
 for index in range(10):
@@ -60,3 +62,22 @@ def draw_board(board):
 
 
 draw_board(grid)
+
+# Loop to try and sink a ship
+turns = 1
+maxTurns = 3
+while turns <= maxTurns:
+    row = input("Select a row: ")
+    column = input("Select a column: ")
+    selection = [(int(row) - 1), (int(column) - 1)]
+
+    # Check for some errors
+    if selection[0] < 1 or selection[0] > 9 or \
+       selection[1] < 1 or selection[1] > 9:
+        print("Invalid selection, try again.")
+    elif grid[selection[0]][selection[1]] == "X":
+        print("You've already selected that target")
+    else:
+        grid[selection[0]][selection[1]] = "X"
+        draw_board(grid)
+        turns += 1

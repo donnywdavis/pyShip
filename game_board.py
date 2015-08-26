a14472da44a44fd0a5bc7efcbff39b18b3dfd49a
Donny Davis <donnywdavis@icloud.com>
2015-08-26 10:58:31 -0400

    Set up methods to load the ships onto the board

--- game_board.py
+++ game_board.py
@@ -4,6 +4,47 @@ __project__ = 'pyShip'
 from random import randint
 
 
+used_positions = []
+
+
+def position_in_use(positions):
+    """
+    Check if a group of positions is already in use for another ship
+    :param positions: A tuple containing potential positions for the current ship
+    :return: Boolean value of whether any of the positions are in use
+    """
+
+    if not positions:
+        return True
+
+    for position in positions:
+        if position in used_positions:
+            return True
+    used_positions.extend(positions)
+    return False
+
+
+def set_ship_position(ships):
+    """
+    Calculate the positions of each ship on the board
+    :param ships: Dictionary of available ships
+    :return: The dictionary of ships with set positions
+    """
+
+    for ship, attr in ships.items():
+        positions = tuple()
+        while position_in_use(positions):
+            start = (randint(2, (11 - attr['length'])), randint(2, 11))
+            if start[1] + attr['length'] > 11:
+                positions = tuple([(x, start[1]) for x in range(start[0], (attr['length'] + start[0]))])
+            else:
+                positions = tuple([(start[0], y) for y in range(start[1], (attr['length'] + start[1]))])
+        else:
+            attr['positions'] = positions
+
+    return ships
+
+
 class GameBoard(object):
     """
     Class to hold variables and methods
@@ -21,6 +62,7 @@ class GameBoard(object):
         """
         Initialize the GameBoard class
         """
+
         self.board = self.build_board()
         self.ships = self.load_ships()
 
@@ -31,13 +73,15 @@ class GameBoard(object):
         """
 
         grid = []
-        row_letter = ['A |', 'B |', 'C |', 'D |', 'E |', 'F |', 'H |', 'I |', 'J |', 'K |']
+        row_letter = [('A', '|'), ('B', '|'), ('C', '|'), ('D', '|'), ('E', '|'), ('F', '|'), ('H', '|'), ('I', '|'),
+                      ('J', '|'), ('K', '|')]
         column_number = [' ', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
         grid.append(column_number)
         grid.append(['-'] * 12)
         for i in range(10):
-            row = [row_letter[i]]
+            row = []
+            row.extend(row_letter[i])
             row.extend([self.OCEAN] * 10)
             grid.append(row)
 
@@ -51,7 +95,8 @@ class GameBoard(object):
         for row in self.board:
             print(' '.join(row))
 
-    def load_ships(self):
+    @staticmethod
+    def load_ships():
         """
         Build dictionary of ships and their attributes
         :return: Dictionary of ships
@@ -84,5 +129,7 @@ class GameBoard(object):
                 'positions': tuple()
             }
         }
+
+        ships = set_ship_position(ships)
 
         return ships

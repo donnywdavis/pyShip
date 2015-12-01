c2bd3942bdd40bd2fb766a66bbb2a3119e3336a3
Donny Davis <donnywdavis@icloud.com>
2015-12-01 14:30:12 -0500

    Check for a hit on a selected position

--- app/player.py
+++ app/player.py
@@ -22,4 +22,29 @@ class Player(object):
         return self.name
 
     def has_remaining_ships(self):
+        """
+        Check if the player has any ships remaining
+
+        :return: Boolean if there are ships remaining
+        """
+
         return True if self.remaining_ships > 0 else False
+
+    def check_for_hit(self, x, y):
+        """
+        Check if the selected position is a hit or not
+
+        :param x: The x coordinate of the position
+        :param y: The y coordinate of the position
+        :return: Boolean if the ship is hit or not
+        """
+
+        for ship in self.ships:
+            if ship.hit(x, y):
+                self.board.mark_hit(x, y)
+                if ship.sunk():
+                    self.remaining_ships -= 1
+                return True
+            else:
+                self.board.mark_miss(x, y)
+                return False

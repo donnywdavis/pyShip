70c23b4c25f072ada4f8e63cdb10c02ea7601402
Donny Davis <donnywdavis@icloud.com>
2015-12-01 09:46:21 -0500

    Initial logic for ships

---
+++ app/ship.py
@@ -0,0 +1,66 @@
+from random import randint
+
+
+def get_ship_positions(ship):
+    """
+    Calculate the positions that a ship will occupy on the board
+
+    :param ship: The ship object to calculate positions for
+    :return: A list with the positions for the ship
+    """
+
+    start = (randint(2, (11 - ship.length)), randint(2, 11))
+    if start[1] + ship.length > 11:
+        positions = tuple([(x, start[1]) for x in range(start[0], (ship.length + start[0]))])
+    else:
+        positions = tuple([(start[0], y) for y in range(start[1], (ship.length + start[1]))])
+
+    return positions
+
+
+def load_ships(number_of_ships):
+    """
+    Build a list of ships that the player will use
+
+    :param number_of_ships: The number of ships to load
+    :return: A list of ship objects for the player
+    """
+    ships_list = (
+        ('battleship', 4),
+        ('carrier', 6),
+        ('submarine', 3),
+        ('destroyer', 3),
+        ('cruiser', 2)
+    )
+    ships = []
+    used_positions = []
+
+    for i in range(number_of_ships):
+        ship = Ship(ships_list[i][0], ships_list[i][1])
+        positions = tuple()
+        while set(used_positions).intersection(positions) or positions == tuple():
+            positions = get_ship_positions(ship)
+        used_positions.append(positions)
+        ship.positions = positions
+        ships.append(ship)
+
+    return ships
+
+
+class Ship(object):
+
+    def __init__(self, name, length):
+        """
+        Initialize a ship object
+
+        :param name: The name of the ship
+        :param length: The length of the ship
+        """
+
+        self.name = name
+        self.length = length
+        self.positions = tuple()
+        self.hits = 0
+
+    def __str__(self):
+        return self.name

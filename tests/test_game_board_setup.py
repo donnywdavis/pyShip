40130e8ca4bfd293440aa9762df46dbf3a68473d
Donny Davis <donnywdavis@icloud.com>
2015-12-01 14:27:18 -0500

    Add tests for positioning on the board

--- tests/test_game_board_setup.py
+++ tests/test_game_board_setup.py
@@ -24,3 +24,32 @@ class GameBoardSetupTests(unittest.TestCase):
             grid.append(row)
 
         self.assertListEqual(self.board.board, grid)
+
+    def test_valid_position_selected(self):
+        self.assertTrue(self.board.position_is_valid('E', 5))
+
+    def test_invalid_row_selected(self):
+        self.assertFalse(self.board.position_is_valid('K', 5))
+
+    def test_no_row_selected(self):
+        self.assertFalse(self.board.position_is_valid('', 5))
+
+    def test_invalid_character_selected_for_row(self):
+        self.assertFalse(self.board.position_is_valid('@', 5))
+
+    def test_invalid_column_selected(self):
+        self.assertFalse(self.board.position_is_valid('E', 11))
+
+    def test_no_column_selected(self):
+        self.assertFalse(self.board.position_is_valid('E', ''))
+
+    def test_invalid_character_selected_for_column(self):
+        self.assertFalse(self.board.position_is_valid('E', 'A'))
+
+    def test_position_marked_as_hit(self):
+        self.board.mark_hit(5, 5)
+        self.assertEqual(self.board.board[5][5], 'H')
+
+    def test_position_marked_as_miss(self):
+        self.board.mark_miss(5, 5)
+        self.assertEqual(self.board.board[5][5], 'M')

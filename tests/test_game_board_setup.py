3fd053380759bd5685caea44cc0c0e114cfab22f
Donny Davis <donnywdavis@icloud.com>
2015-12-01 23:03:23 -0500

    Update game board tests to account for coloring codes

--- tests/test_game_board_setup.py
+++ tests/test_game_board_setup.py
@@ -1,5 +1,10 @@
 import unittest
+from colorama import init, Fore
 from app.game_board import GameBoard
+import string
+import random
+
+init()
 
 
 class GameBoardSetupTests(unittest.TestCase):
@@ -12,21 +17,22 @@ class GameBoardSetupTests(unittest.TestCase):
 
     def test_game_board_has_10_by_10_grid(self):
         grid = []
-        columns = [' ', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        rows = [('A', '|'), ('B', '|'), ('C', '|'), ('D', '|'), ('E', '|'), ('F', '|'), ('G', '|'), ('H', '|'),
-                ('I', '|'), ('J', '|')]
+        columns = [Fore.WHITE + ' ', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        rows = [(Fore.WHITE + 'A', '|'), (Fore.WHITE + 'B', '|'), (Fore.WHITE + 'C', '|'), (Fore.WHITE + 'D', '|'),
+                (Fore.WHITE + 'E', '|'), (Fore.WHITE + 'F', '|'), (Fore.WHITE + 'G', '|'), (Fore.WHITE + 'H', '|'),
+                (Fore.WHITE + 'I', '|'), (Fore.WHITE + 'J', '|')]
         grid.append(columns)
-        grid.append(['-'] * 12)
+        grid.append([Fore.WHITE + '-'] * 12)
         for i in range(10):
             row = []
             row.extend(rows[i])
-            row.extend(['O'] * 10)
+            row.extend([self.board.__class__.OCEAN] * 10)
             grid.append(row)
 
         self.assertListEqual(self.board.board, grid)
 
     def test_valid_position_selected(self):
-        self.assertTrue(self.board.position_is_valid('E', 5))
+        self.assertTrue(self.board.position_is_valid(random.choice(string.ascii_uppercase[:10]), 5))
 
     def test_invalid_row_selected(self):
         self.assertFalse(self.board.position_is_valid('K', 5))
@@ -38,18 +44,18 @@ class GameBoardSetupTests(unittest.TestCase):
         self.assertFalse(self.board.position_is_valid('@', 5))
 
     def test_invalid_column_selected(self):
-        self.assertFalse(self.board.position_is_valid('E', 11))
+        self.assertFalse(self.board.position_is_valid(random.choice(string.ascii_uppercase[:10]), 11))
 
     def test_no_column_selected(self):
-        self.assertFalse(self.board.position_is_valid('E', ''))
+        self.assertFalse(self.board.position_is_valid(random.choice(string.ascii_uppercase[:10]), ''))
 
     def test_invalid_character_selected_for_column(self):
-        self.assertFalse(self.board.position_is_valid('E', 'A'))
+        self.assertFalse(self.board.position_is_valid(random.choice(string.ascii_uppercase[:10]), 'A'))
 
     def test_position_marked_as_hit(self):
         self.board.mark_hit(5, 5)
-        self.assertEqual(self.board.board[5][5], 'H')
+        self.assertEqual(self.board.board[5][5], self.board.__class__.HIT)
 
     def test_position_marked_as_miss(self):
         self.board.mark_miss(5, 5)
-        self.assertEqual(self.board.board[5][5], 'M')
+        self.assertEqual(self.board.board[5][5], self.board.__class__.MISS)
